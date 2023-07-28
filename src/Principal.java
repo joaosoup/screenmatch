b7d1696f136d0e8bbe25ef8bdcf5925a4c6e3fe4
João Victor <126724937+joaosoup@users.noreply.github.com>
2023-07-28 11:10:33 -0300

    Criação de packages e encapsulamento do código

--- src/Principal.java
+++ src/Principal.java
@@ -1,16 +1,23 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "Por lugares incríveis";
-        meuFilme.anoDeLancamento = 2019;
-        meuFilme.duracaoEmMinuto = 180;
+        meuFilme.setNome("Por lugares incríveis");
+        meuFilme.setAnoDeLancamento(2020);
+        meuFilme.setDuracaoEmMinuto(108);
+        meuFilme.setIncluidoNoPlano(true);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(10);
         meuFilme.avalia(6);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
+        /*meuFilme.somaDasAvaliacoes = 10;
+        meuFilme.totalDeAvaliacoes = 1;
+        System.out.println(meuFilme.pegaMedia());
+        */
     }
 }
